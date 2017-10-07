f757b7c05700e7e78312dde2dc38f6277dd1b0f6
NLOG <sorisem4106@naver.com>
2017-10-07 15:23:13 +0900

    Update Main.php

--- src/nlog/LastSignChangeEvent/Main.php
+++ src/nlog/LastSignChangeEvent/Main.php
@@ -11,6 +11,14 @@ use pocketmine\network\mcpe\protocol\MovePlayerPacket;
 use pocketmine\network\mcpe\protocol\InteractPacket;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\network\mcpe\protocol\PlayerActionPacket;
+use pocketmine\Player;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+use pocketmine\network\mcpe\protocol\ServerSettingsResponsePacket;
+use pocketmine\network\mcpe\protocol\TextPacket;
+use pocketmine\network\mcpe\protocol\ContainerOpenPacket;
+use pocketmine\network\mcpe\protocol\ContainerClosePacket;
+use pocketmine\network\mcpe\protocol\AnimatePacket;
+use pocketmine\network\mcpe\protocol\BookEditPacket;
 
 class Main extends PluginBase implements Listener {
 	
@@ -22,19 +30,34 @@ class Main extends PluginBase implements Listener {
   	MainLogger::getLogger()->info("LastSignChangeEvent 플러그인 활성화");
   }
   
+  public function check(Player $player) {
+  	if (isset($this->writing[$player->getId()])) {
+  		$this->onProcess($player);
+  	}
+  }
+  
+  public function onProcess(Player $player) {
+  	unset($this->writing[$player->getId()]);
+  	$ev = new LastSignChangeEvent($this->writing[$player->getId()], $player);
+  	$this->getServer()->getPluginManager()->callEvent($ev);
+  	if ($ev->isCancelled()) {
+  		return;
+  	}
+  	$ev->getSign()->onUpdate();
+  }
+  
   public function onRecieved (DataPacketReceiveEvent $ev) {
   	$packet = $ev->getPacket();
-  	if ($packet instanceof MovePlayerPacket || $packet instanceof InteractPacket || $packet instanceof PlayerActionPacket) {
-  		if (isset($this->writing[$ev->getPlayer()->getId()])) {
-  			unset($this->writing[$ev->getPlayer()->getId()]);
-  			$ev = new LastSignChangeEvent($this->writing[$ev->getPlayer()->getId()], $ev->getPlayer());
-  			$this->getServer()->getPluginManager()->callEvent($ev);
-  			if ($ev->isCancelled()) {
-  				return;
-  			}
-  			$ev->getSign()->onUpdate();
-  			return;
-  		}
+  	if ($packet instanceof MovePlayerPacket || 
+  		$packet instanceof InteractPacket || 
+  		$packet instanceof PlayerActionPacket || 
+  		$packet instanceof ModalFormResponsePacket ||
+  		$packet instanceof ServerSettingsResponsePacket ||
+  		$packet instanceof TextPacket ||
+  		$packet instanceof ContainerOpenPacket || $packet instanceof ContainerClosePacket ||
+  		/*$packet instanceof AnimatePacket*/ $packet instanceof BookEditPacket
+  		) {
+  		self::check($ev->getPlayer());
   	}
   }
   
